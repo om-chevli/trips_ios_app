a55793f4e4797f5fe2846a22a4175712b80ce034
om <omchevli@gmail.com>
2023-03-06 19:11:46 -0500

    Initial Commit

---
+++ Group11_Final/Group11_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Group11_FinalApp.swift
+//  Group11_Final
+//
+//  Created by Om Chevli on 2023-03-06.
+//
+
+import SwiftUI
+
+@main
+struct Group11_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
